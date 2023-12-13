c3efc1dc2bc9cef60e64755bdc33c35adc8a1dae
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2023-12-13 23:58:30 +0500

    User Delete Operation Added

--- Application/routes/user.py
+++ Application/routes/user.py
@@ -32,14 +32,14 @@ def register_user(request: Schema.User, db: Session = Depends(Database.get_db)):
     return f'User registered Successfully \n {request}'
 
 
-# # === Delete Existing User ===
-# @router.delete('/{ID}', status_code=204)
-# def delete_user(ID, db: Session = Depends(Database.get_db)):
-#     user = db.query(Models.User).filter(Models.User.ID == ID)
-#     if not user.first():
-#         raise HTTPException(status_code=404, 
-#         detail=f'User with {ID} did not Exist')
+# === Delete Existing User ===
+@router.delete('/{ID}', status_code=204)
+def delete_user(ID, db: Session = Depends(Database.get_db)):
+    user = db.query(Models.User).filter(Models.User.ID == ID)
+    if not user.first():
+        raise HTTPException(status_code=404, 
+        detail=f'User with {ID} did not Exist')
     
-#     user.delete(synchronize_session = False)
-#     db.commit()
-#     return f'User with ID: {ID} Deleted'
+    user.delete(synchronize_session = False)
+    db.commit()
+    return f'User with ID: {ID} Deleted'
